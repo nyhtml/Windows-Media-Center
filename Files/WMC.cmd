1096ef7efd48274b030052cbfdbd65428d5c2652
Steve Pringle <nyhtml@users.noreply.github.com>
2025-07-11 19:19:54 -0400

    WMC.cmd

---
+++ Files/WMC.cmd
@@ -0,0 +1,102 @@
+@ECHO OFF
+COLOR 0F && CLS
+TITLE WMC v%Year%
+
+:: Get current year using PowerShell (modern systems)
+FOR /F %%i IN ('powershell -NoProfile -Command "Get-Date -Format yyyy"') DO SET Year=%%i
+
+:SELECT
+CLS
+ECHO ----------------------------------------------------------------
+ECHO                W.M.C. - Windows Media Center %Year%
+ECHO ----------------------------------------------------------------
+ECHO.
+ECHO                 Windows Media Center (WMC) can
+ECHO                   aid you in playing slideshows,
+ECHO                    videos, music, and more
+ECHO                     from numerous sources
+ECHO                       on the Windows OS.
+ECHO.
+ECHO -----------------------------------------------------------------
+ECHO    Please make sure that you are connected to the Internet.
+ECHO     Looking for older instances of Office in the Control Panel
+ECHO               If found, the install will fail.
+ECHO -----------------------------------------------------------------
+ECHO    Copyright (c) %Year% Stephan Pringle. All rights reserved.
+ECHO -----------------------------------------------------------------
+ECHO.
+ECHO  Install:
+ECHO  1. Windows Media Center (Black)
+ECHO  2. Windows Media Center (Blue)
+ECHO  3. Windows Media Center (Green)
+ECHO  4. Windows Media Center (Grey)
+ECHO  5. Windows Media Center (Red)
+ECHO  6. Windows Media Center (Yellow)
+ECHO.
+ECHO  0. Quit
+ECHO.
+
+:: Prompt user
+SET /P ChoosedLanguage=Enter a number and press ENTER key or 0 to quit: 
+
+:: Validate numeric input
+SETLOCAL ENABLEDELAYEDEXPANSION
+SET /A Dummy=%ChoosedLanguage% >NUL 2>&1
+IF ERRORLEVEL 1 (
+    ENDLOCAL
+    ECHO Invalid selection. Please enter a valid number.
+    TIMEOUT /T 2 >NUL
+    GOTO SELECT
+)
+ENDLOCAL
+
+:: Routing logic
+IF "%ChoosedLanguage%"=="0" GOTO E
+IF %ChoosedLanguage% GEQ 1 IF %ChoosedLanguage% LEQ 6 GOTO %ChoosedLanguage%
+
+:: Catch invalid values
+ECHO Invalid selection. Please enter a valid option.
+TIMEOUT /T 2 >NUL
+GOTO SELECT
+
+:: Installer Labels
+:1
+ECHO Installing WMC with the Black theme...
+CALL black.cmd
+GOTO DONE
+
+:2
+ECHO Installing WMC with the Blue theme...
+CALL InstallerBLUE.cmd
+GOTO DONE
+
+:3
+ECHO Installing WMC with the Green theme...
+CALL InstallerGREEN.cmd
+GOTO DONE
+
+:4
+ECHO Installing WMC with the Grey theme...
+CALL InstallerGREY.cmd
+GOTO DONE
+
+:5
+ECHO Installing WMC with the Red theme...
+CALL InstallerRED.cmd
+GOTO DONE
+
+:6
+ECHO Installing WMC with the Yellow theme...
+CALL InstallerYELLOW.cmd
+GOTO DONE
+
+:DONE
+ECHO.
+ECHO Returning to main menu...
+TIMEOUT /T 2 >NUL
+GOTO SELECT
+
+:E
+ECHO Exiting WMC installer...
+TIMEOUT /T 1 >NUL
+EXIT
